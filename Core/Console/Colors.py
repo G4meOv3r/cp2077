d889101bc1793f9e5c4682ad218a3357960b2e99
G4meOv3r <boyarnikov.sasha@gmail.com>
2021-08-18 21:14:04 +0300

    Constants of colors and effects

---
+++ Core/Console/Colors.py
@@ -0,0 +1,30 @@
+class Colors {
+    FG_BLACK            =   "30" 
+    FG_RED              =   "31"
+    FG_GREEN            =   "32"
+    FG_YELLOW           =   "33"
+    FG_BLUE             =   "34"
+    FG_PURPLE           =   "35"
+    FG_CIAN             =   "36"
+    FG_WHITE            =   "37"
+
+    BG_BLACK            =   "40" 
+    BG_RED              =   "41"
+    BG_GREEN            =   "42"
+    BG_YELLOW           =   "43"
+    BG_BLUE             =   "44"
+    BG_PURPLE           =   "45"
+    BG_CIAN             =   "46"
+    BG_WHITE            =   "47"
+}
+
+class Effects {
+    EFFECT_NONE         =   "0"
+    EFFECT_BOLD         =   "1"
+    EFFECT_THIN         =   "2"
+    EFFECT_ITALIC       =   "3"
+    EFFECT_UNDERLINE    =   "4"
+    EFFECT_BLINK        =   "5"
+    EFFECT_BLINK_FAST   =   "6"
+    EFFECT_SWAP         =   "7"
+}
